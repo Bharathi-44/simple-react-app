c82475e8d11b238b1a105f4bd839c27c94afd6ec
Bharathi-44 <bharathi.k.2000m@gmail.com>
2023-02-02 22:22:52 +0530

    first commit

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+//this is function component
+
+function Nav()
+{
+    return(
+        <nav class="d-flex text-center p-3">
+            <img src="./reactimg.png" width="70px" height="70px" alt="no" className="mr-1" />
+            <h3 class="texth3 mr-auto">React Facts</h3>
+            <h3 className="texth4">React Course Project-1</h3>
+        </nav>
+    )
+}
+
+export default Nav;
